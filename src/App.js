39b4260cd1879fbcfbd8c7d12f4ff8f9045e8bcc
makro89 <mgalic99@gmail.com>
2023-05-19 15:18:54 +0200

    promjene

--- src/App.js
+++ src/App.js
@@ -1,28 +1,45 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
-import Chat from "./Components/Chat";
-import Login from "./Components/Login";
-import { Navigate } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
+import About from "./Components/About";
+import Chat from "./components/Chat";
+import Header from "./Components/Header";
+import Login from "./components/Login";
+import Sidebar from "./Components/Sidebar";
+import "./App.css";
 
 class App extends React.Component{
+
   constructor(props){
     super(props);
     this.state={username: ""};
   }
-
-handleLogin = (username) =>{
-  this.setState({username})
-}
-
-  render(){
-    return    ( 
-    <Routes>
-      <Route path="/" element={this.state.username ?<Chat username={this.state.username} /> : <Navigate to="/login" />}/>
-      <Route path="/login" element={<Login onLogin={this.handleLogin}/>} />
-    </Routes>
-    )
+  
+  handleLogin = (username) =>{
+    this.setState({username})
+  }
+  
+  toggleSidebar = () =>{
+    this.sidebar.ToggleSidebar();
   }
 
+  handleLogout =() =>{
+    this.setState({username: ""});
+  }
+
+  render(){
+    return   ( 
+       <div className="App">  
+         <Header toggleSidebar={this.toggleSidebar} username={this.state.username} handleLogout={this.handleLogout} /> 
+         <Sidebar ref={(reference)=> this.sidebar = reference}/> 
+          <Routes>
+              <Route path="/" 
+              element={ this.state.username ? <Chat username={this.state.username}/> : <Navigate to="/login" />} />
+              <Route path="/login" element={<Login onLogin={this.handleLogin} username={this.state.username}/>} />
+              <Route path="/about" element={this.state.username ? <About />: <Navigate to="/login" /> } />
+            </Routes>
+    </div>
+    )
+  }
 }
 
 export default App;
